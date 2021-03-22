90b2d38a6f7e13204a601a12835d74d4e4b69067
thohohong <so0426yeon@gmail.com>
2021-03-22 17:53:54 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://sandyk:asdf1234@reactbasic.css5d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
